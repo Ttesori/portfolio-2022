7fd1319dca5212ba28f673b5e65be9b2a8c3a109
Toni Tesori <tt@tonitesori.dev>
2025-04-04 10:33:57 -0600

    Fix active links on scroll

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const els = {
   navToggle: document.querySelector('.nav-toggle'),
-  navList: document.querySelector('.nav__list')
-}
+  navList: document.querySelector('.nav__list'),
+  navLinks: document.querySelectorAll(".link--section"),
+};
 
 const classes = {
   navToggle: 'nav-toggle--display',
@@ -9,18 +10,18 @@ const classes = {
   listClosed: 'nav__list--closed',
   listOpen: 'nav__list--open',
   linkActive: 'nav__link--active'
-}
+};
 
 const init = () => {
   setUpMenu();
   toggleMenu();
-  addObserver();
-}
+  activeOnScroll();
+};
 
 const setUpMenu = () => {
   els.navToggle.addEventListener('click', toggleMenu);
   els.navToggle.classList.add(classes.navToggle);
-}
+};
 
 const toggleMenu = () => {
   if (els.navList.classList.contains(classes.listClosed)) {
@@ -28,33 +29,39 @@ const toggleMenu = () => {
   } else {
     closeMenu();
   }
-}
+};
 
 const closeMenu = () => {
   els.navList.classList.remove(classes.listOpen);
   els.navList.classList.add(classes.listClosed);
   els.navToggle.classList.remove(classes.toggleOpen);
-}
+};
 const openMenu = () => {
   els.navList.classList.remove('nav__list--closed');
   els.navList.classList.add(classes.listOpen);
   els.navToggle.classList.add(classes.toggleOpen);
-}
+};
 
-const addObserver = () => {
-  const observer = new IntersectionObserver(entries => entries.forEach(entry => {
-    const id = entry.target.getAttribute('id');
-    if (entry.isIntersecting && window.scrollY > 0) {
-      document.querySelector(`.${classes.linkActive}`).classList.remove(classes.linkActive);
-      document.querySelector(`nav li a[href="#${id}"]`).classList.add(classes.linkActive);
-    }
-  }), {
-    threshold: window.innerWidth > 980 ? .75 : .2, // 75% or 10% of el is visible
-    rootMargin: '-200px 0px 0px 0px', // top margin is 200px from the top
+const activeOnScroll = () => {
+  let mainNavLinks = els.navLinks;
+
+  window.addEventListener("scroll", event => {
+    let fromTop = window.scrollY - 50;
+
+    mainNavLinks.forEach(link => {
+      let hash = new URL(link.href).hash;
+      let section = document.querySelector(hash);
+      if (
+        section.offsetTop <= fromTop + 200 &&
+        section.offsetTop + section.offsetHeight > fromTop + 200
+      ) {
+        link.classList.add(classes.linkActive);
+      } else {
+        link.classList.remove(classes.linkActive);
+      }
+    });
   });
-  document.querySelectorAll('section[id]').forEach((section) => {
-    observer.observe(section);
-  })
-}
+
+};
 
 window.addEventListener('DOMContentLoaded', init);
